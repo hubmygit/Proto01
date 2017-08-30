3c945a454d41f46ef023a190e9f8e347591eee16
haris <haris@testing.gr>
2017-08-30 09:26:52 +0300

    Init actions

--- Protocol/MasterForm.cs
+++ Protocol/MasterForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Protocol
 {
-    public partial class Form1 : Form
+    public partial class MasterForm : Form
     {
-        public Form1()
+        public MasterForm()
         {
             InitializeComponent();
 
