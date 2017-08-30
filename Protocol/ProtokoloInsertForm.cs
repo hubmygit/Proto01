8662b9a93e32a431cb21c51294103c954ccd4c7e
haris <haris@testing.gr>
2017-08-30 11:39:43 +0300

    Init Actions part 4

--- Protocol/ProtokoloInsertForm.cs
+++ Protocol/ProtokoloInsertForm.cs
@@ -15,6 +15,38 @@ namespace Protocol
         public ProtokoloInsertForm()
         {
             InitializeComponent();
+
+            //ToDo: From get values from database
+            cbProtokoloKind.Items.AddRange(new object[] {
+            "",
+            "Εισερχόμενα",
+            "Εξερχόμενα"});
+        }
+
+        private void cbProtokoloKind_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbProtokoloKind.Text == "Εισερχόμενα")
+            {
+                panelInbox.Visible = true;
+                panelInbox.Location = new Point(12, 110);
+                panelOutbox.Visible = false;
+            }
+            else if (cbProtokoloKind.Text == "Εξερχόμενα")
+            {
+                panelInbox.Visible = false;
+                panelOutbox.Visible = true;
+                panelOutbox.Location = new Point(12, 110);
+            }
+            else
+            {
+                panelInbox.Visible = false;
+                panelOutbox.Visible = false;
+            }
+        }
+
+        private void panelInbox_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
