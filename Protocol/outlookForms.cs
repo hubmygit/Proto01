6267ef8eb77c64d036d5ae89d310b0a4e84ed916
haris <haris@testing.gr>
2017-09-20 10:02:47 +0300

    outlook reference & outlookForm

---
+++ Protocol/outlookForms.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace Protocol
+{
+    class outlookForms
+    {
+    }
+}
