b9f526adfda1c44728f2165b4723f06e985347b2
haris <haris@testing.gr>
2017-09-29 13:47:18 +0300

    add savedDatasources.cs

---
+++ Protocol/SavedDatasources.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace Protocol
+{
+        public partial class SavedDatasources
+        {
+            public String DBField;
+            public Control FormField;
+            public Binding BindingProc;
+
+            public SavedDatasources(String DBField, Control FormField, Binding BindingProc)
+            {
+                this.DBField = DBField;
+                this.FormField = FormField;
+                this.BindingProc = BindingProc;
+            }
+        }
+    
+}
