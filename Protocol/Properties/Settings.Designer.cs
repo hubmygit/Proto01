286630689529f1ee76e1d15202eafcdb36a55e20
haris <haris@testing.gr>
2017-10-02 09:14:47 +0300

    add dataset, new connection string

--- Protocol/Properties/Settings.Designer.cs
+++ Protocol/Properties/Settings.Designer.cs
@@ -22,5 +22,16 @@ namespace Protocol.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=AVINDOMC\\SQLSERVERR2;Initial Catalog=GramV3-Dev;Persist Security Info" +
+            "=True;User ID=GramV3;Password=8093570")]
+        public string GramV3_DevConnectionString {
+            get {
+                return ((string)(this["GramV3_DevConnectionString"]));
+            }
+        }
     }
 }
---
+++ Protocol/_GramV3_DevDataSet_Contact.cs
@@ -0,0 +1,15 @@
+﻿namespace Protocol
+{
+
+
+    public partial class _GramV3_DevDataSet_Contact
+    {
+        partial class ContactsDataTable
+        {
+        }
+
+        partial class UpdateKey1DataTable
+        {
+        }
+    }
+}
